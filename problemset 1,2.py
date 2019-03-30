766d44b11d728590f73dac6c228dcd430dfebf17
sravanikodipelly <sravanikodipelly@gmail.com>
2019-03-30 22:36:57 +0530

    Changed monthly payment rate to monthly pay rate

--- problemset 1,2.py
+++ problemset 1,2.py
@@ -1,6 +1,6 @@
 outStandingBlanace = float(raw_input('Enter the outstanding balance on your credit card: '))
 annualInterestRate= float(raw_input('Enter the annual credit card interest rate as a decimal: '))
-minMonthlyPaymentRate= float(raw_input('Enter the minimum monthly payment rate as a decimal: '))
+minMonthlyPaymentRate= float(raw_input('Enter the minimum monthly pay rate as a decimal: '))
 for i in range(1,13):
     print 'Month: ', i
     minMonthlyPayment = round(minMonthlyPaymentRate * outStandingBlanace, 2)
